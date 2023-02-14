f0d757154e0488fd80a7392dcd35aa0cff717649
mattjercino <matthew@jercinovic.dev>
2023-02-14 13:22:11 +1100

    added cors policy

--- KioskApi/Program.cs
+++ KioskApi/Program.cs
@@ -1,11 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
-
+var MyPolicy = "MyPolicy";
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyPolicy, policy =>
+    {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyMethod();
+        policy.AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +27,8 @@ if (app.Environment.IsDevelopment())
 }
 
 //app.UseHttpsRedirection();
+
+app.UseCors(MyPolicy);
 
 app.UseAuthorization();
 
